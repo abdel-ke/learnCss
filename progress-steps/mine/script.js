f9a0b0928cf194fc2635e1dcc7347eacd60c1b1e
Abdelhamid El kemmal <abdel-ke@e3r5p8.1337.ma>
2023-08-03 20:24:28 +0100

    progress-steps

---
+++ progress-steps/mine/script.js
@@ -0,0 +1,44 @@
+const skyBlue = "#3498db";
+const gray = "#e0e0e0";
+let pos = 1;
+let posLine = 0;
+let number = document.getElementsByClassName("number");
+let line = document.getElementsByClassName("line");
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
+
+function Next() {
+    console.log(pos)
+    if (pos > 3 || pos < 0)
+        return;
+        number[pos++].style.borderColor = skyBlue;
+        line[posLine++].style.backgroundColor = skyBlue;
+        checkBtn();
+}
+
+function Prev() {
+    console.log(pos)
+    if (pos > 4 || pos <= 1)
+        return;
+        number[--pos].style.borderColor = gray;
+        line[--posLine].style.backgroundColor = gray;
+        checkBtn();
+}
+
+function checkBtn() {
+    if (pos >= 1 && pos < 4)
+    {
+        prevBtn.style.backgroundColor = skyBlue;
+        nextBtn.style.backgroundColor = skyBlue;
+    }
+    if (pos == 1)
+    {
+        prevBtn.style.backgroundColor = gray;
+        nextBtn.style.backgroundColor = skyBlue;
+    }
+    if (pos == 4)
+    {
+        prevBtn.style.backgroundColor = skyBlue;
+        nextBtn.style.backgroundColor = gray;
+    }
+}
